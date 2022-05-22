a4f9d2d9b1cf7c8e716ee75fa2a29f1731e4044b
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-22 21:32:07 +0200

    Wrote a small program to listen for events on Capture device which avoids wasting CPU cycles. The program code is pretty bad, do not look.

---
+++ scripts/microphone/microphone.c
@@ -0,0 +1,110 @@
+// gcc -Wall -Wextra -lasound -o microphone microphone.c
+#include <alsa/asoundlib.h>
+
+static char card[] = "default";
+static char command[512];
+static char script[] = ".sh";
+
+// Handle errors nicely (ty amixer source code)
+static void error(const char *fmt, ...) {
+    va_list va;
+
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    fprintf(stderr, "\n");
+    va_end(va);
+}
+
+// Simple utility to get a device name from a snd_hctl_elem_t struct
+static const char *get_dev_name(snd_hctl_elem_t *helem) {
+    snd_ctl_elem_id_t *id;
+    snd_ctl_elem_id_alloca(&id);
+    snd_hctl_elem_get_id(helem, id);
+
+    const char *device_name = snd_ctl_elem_id_get_name(id);
+    return device_name;
+}
+
+static int element_callback(snd_hctl_elem_t *elem, unsigned int mask) {
+
+    // Whenever the value changes, call the function to handle its change
+    if (mask & SND_CTL_EVENT_MASK_VALUE)
+        // For simplicity (and because ALSA documentation is like unicorns, it
+        // doesn't exist), just use a script to handle printing for polybar
+        system(command);
+    return 0;
+}
+
+static int register_events_cb(snd_hctl_t *ctl, unsigned int mask,
+                              snd_hctl_elem_t *elem) {
+
+    // When adding callbacks to events
+    if (mask & SND_CTL_EVENT_MASK_ADD) {
+        const char *device_name = get_dev_name(elem);
+        size_t len = strlen(device_name);
+
+        // If the device name is either Capture Switch or Capture Volume
+        if (!strncmp(device_name, "Capture Switch", len) ||
+            !strncmp(device_name, "Capture Volume", len)) {
+            fprintf(stderr, "Added: %s\n", device_name);
+
+            // Add a callback (we do not care about other devices)
+            snd_hctl_elem_set_callback(elem, element_callback);
+        }
+    }
+    return 0;
+}
+
+static int subscribe() {
+    snd_hctl_t *handle;
+    snd_hctl_elem_t *helem;
+    int err;
+
+    // Open an handle
+    if ((err = snd_hctl_open(&handle, card, 0)) < 0) {
+        error("Control %s open error: %s\n", card, snd_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // Set the callback
+    snd_hctl_set_callback(handle, register_events_cb);
+    if ((err = snd_hctl_load(handle)) < 0) {
+        error("Control %s hbuild error: %s\n", card, snd_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // Start event cycle
+    while (1) {
+
+        // Whenever an event comes, handle it with the callbacks
+        int res = snd_hctl_wait(handle, -1);
+        if (res >= 0) {
+            fprintf(stderr, "Poll ok: %i\n", res);
+            res = snd_hctl_handle_events(handle);
+            assert(res > 0);
+        }
+    }
+    snd_hctl_close(handle);
+    return 0;
+}
+
+int main() {
+    int res;
+    // Get directory of script
+    if ((res = readlink("/proc/self/exe", command,
+                        sizeof(command) - sizeof(script))) < 0) {
+        error("Could not get directory of program!");
+        exit(EXIT_FAILURE);
+    }
+
+    // Assumes that this file is compiled as "microphone" and that the script
+    // you want to execute on change is called "microphone.sh", and both are in
+    // the same directory
+    size_t len = strlen(command);
+    command[len] = '.';
+    command[len + 1] = 's';
+    command[len + 2] = 'h';
+    command[len + 3] = '\0';
+
+    subscribe();
+}
---
+++ scripts/microphone/microphone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LOGO_ON=" "
+LOGO_OFF=""
+
+DEFAULT_SOURCE="Capture"
+MUTED=$(amixer get "$DEFAULT_SOURCE" | awk '/Front Left:/ {print $6; exit}');
+RUNNING=$(cat /proc/asound/card*/pcm*/sub*/status | awk '/RUNNING/' | wc -l | tr -d "\n");
+
+if (( RUNNING > 0  )); then 
+    if [ "$MUTED" = "[on]" ]; then
+      echo $LOGO_ON
+    else 
+      echo "%{F#f25287}$LOGO_OFF%{F-}"
+    fi
+else
+    echo ""
+fi
