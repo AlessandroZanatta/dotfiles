16cb78cf545f30700fab1aa52e9bff4d3ec4069e
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-23 13:41:33 +0200

    Stonger blur. Rewrote microphone logic.

--- scripts/microphone/microphone.sh
+++ scripts/microphone/microphone.sh
@@ -4,15 +4,15 @@ LOGO_ON=" "
 LOGO_OFF=""
 
 DEFAULT_SOURCE="Capture"
-MUTED=$(amixer get "$DEFAULT_SOURCE" | awk '/Front Left:/ {print $6; exit}');
-RUNNING=$(cat /proc/asound/card*/pcm*/sub*/status | awk '/RUNNING/' | wc -l | tr -d "\n");
+MUTED=$(amixer get "$DEFAULT_SOURCE" | awk '/Front Left:/ {print $6; exit}')
+RUNNING=$(cat /proc/asound/card*/pcm*/sub*/status | awk '/RUNNING/' | wc -l | tr -d "\n")
 
-if (( RUNNING > 0  )); then 
-    if [ "$MUTED" = "[on]" ]; then
-      echo $LOGO_ON
-    else 
-      echo "%{F#f25287}$LOGO_OFF%{F-}"
-    fi
+if [ "$RUNNING" -gt 0 ] || [ "$MUTED" != "[on]" ]; then
+  if [ "$MUTED" = "[on]" ]; then
+    echo "$LOGO_ON"
+  else
+    echo "%{F#f25287}$LOGO_OFF%{F-}"
+  fi
 else
-    echo ""
+  echo ""
 fi
