5f095b9f4d89d90c351ba121f21c1a3b0939639d
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-28 10:04:01 +0200

    Mapped coc commands

--- misc/nvim_lua_custom/init.lua
+++ misc/nvim_lua_custom/init.lua
@@ -1,2 +1,17 @@
 -- GLOBALS
 vim.g.coc_config_home = "/home/kalex/dotfiles/misc/coc/"
+
+-- FUNCTIONS
+vim.cmd [[ 
+  Function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+Endfunction
+]]
+
+-- AUTOCMDS
+-- vim.cmd "autocmd CursorHold * silent call CocActionAsync('highlight')"
+vim.api.nvim_create_autocmd("CursorHold", {
+   pattern = "*",
+   command = "silent call CocActionAsync('highlight')",
+})
--- misc/nvim_lua_custom/mappings.lua
+++ misc/nvim_lua_custom/mappings.lua
@@ -4,13 +4,47 @@ M.disabled = {
    n = {
       ["<leader>q"] = "",
       ["K"] = "",
+      ["<leader>f"] = "",
    },
 }
 
-M.keys = {
+M.utils = {
    n = {
       ["K"] = { ":Man<CR>", "Open manual" },
       ["<leader>q"] = { ":q<CR>", "Close" },
+   },
+}
+
+M.coc = {
+   n = {
+      ["<leader>rn"] = { "<Plug>(coc-rename)", "Coc: rename instances" },
+      ["<leader>a"] = { "<Plug>(coc-codeaction-selected)", "Coc: apply code action on region" },
+      ["<leader>ac"] = { "<Plug>(coc-codeaction)", "Coc: apply code action on buffer" },
+      ["<leader>f"] = { "<Plug>(coc-fix-current)", "Coc: apply auto fix" },
+   },
+}
+
+M.coc_silent = {
+   mode_opts = { silent = true },
+   n = {
+      ["[g"] = { "<Plug>(coc-diagnostic-prev)", "Coc: goto previous diagnostic" },
+      ["]g"] = { "<Plug>(coc-diagnostic-next)", "Coc: goto next diagnostic" },
+      ["gd"] = { "<Plug>(coc-definition)", "Coc: goto definition" },
+      ["gy"] = { "<Plug>(coc-type-definition)", "Coc: goto type definition" },
+      ["gi"] = { "<Plug>(coc-implementation)", "Coc: goto implementation" },
+      ["gr"] = { "<Plug>(coc-references)", "Coc: goto references" },
+      ["<leader>cl"] = { "<Plug>(coc-codelens-action)", "Coc: code lens action" },
+   },
+}
+
+M.coc_silent_expr = {
+   mode_opts = { silent = true, expr = true },
+   i = {
+      ["<TAB>"] = {
+         'pumvisible() ? "\\<C-n>" : CheckBackspace() ? "\\<TAB>" : coc#refresh()',
+         "Coc: trigger autocompletion and navigate",
+      },
+      ["<c-space>"] = { "coc#refresh()", "Coc: trigger completion" },
    },
 }
 
