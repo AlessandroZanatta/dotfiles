55b3b88d9ad2ccb7ba04ba7d2b3ff68b0d192a57
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-05-28 10:17:28 +0200

    Fixed autocmd which was not being added (reason unknown).

--- misc/nvim_lua_custom/init.lua
+++ misc/nvim_lua_custom/init.lua
@@ -1,17 +1,15 @@
+-- AUTOCMDS
+vim.api.nvim_create_autocmd("CursorHold", {
+   command = "silent call CocActionAsync('highlight')",
+})
+
 -- GLOBALS
 vim.g.coc_config_home = "/home/kalex/dotfiles/misc/coc/"
 
 -- FUNCTIONS
 vim.cmd [[ 
   Function! CheckBackspace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-Endfunction
+    let col = col('.') - 1
+    return !col || getline('.')[col - 1]  =~# '\s'
+  Endfunction
 ]]
-
--- AUTOCMDS
--- vim.cmd "autocmd CursorHold * silent call CocActionAsync('highlight')"
-vim.api.nvim_create_autocmd("CursorHold", {
-   pattern = "*",
-   command = "silent call CocActionAsync('highlight')",
-})
