d298a749547115e684d7d299696054ac634bd0ac
AlessandroZanatta <alessandro.zanatta.lav@gmail.com>
2022-06-02 10:19:19 +0200

    Better battery notifications. Kitty does not open urls with a simple click. Minor polybar aesthetic change

--- scripts/battery_notify.sh
+++ scripts/battery_notify.sh
@@ -2,11 +2,30 @@
 
 # Arbitrary, but need to be unique
 MSGTAG="my_battery_notification"
+FULL_AT=98
+
+DEBUG=false
 
 # Power is always a multiple of 10
 POWER=$(cat /sys/class/power_supply/BAT0/capacity)
 POWER_10=$(echo "$POWER" | awk '{print int($1 / 10 + 0.5) * 10}')
 PLUGGED=$(cat /sys/class/power_supply/BAT0/status)
+
+# My battery doesn't seem to output events when changing current capacity
+# Therefore, I just have to use a cronjob. The cronjob passes "cron" as the
+# first argument to avoid having a notification about battery level every 5m
+# Also, when full and plugged my battery sometimes emits an event. The last
+# condition in or is to check for that. Battery events pass in as first argument
+# "battery_event"
+if [[ $# -gt 0 ]] \
+  && ([[ "$1" == "cron" ]] && [ "$PLUGGED" != "Discharging" ] && ((POWER > 20)) \
+    || ([[ "$1" == "battery_event" ]] && ((POWER > FULL_AT)))); then
+  exit 0
+fi
+
+if ((POWER >= FULL_AT)); then
+  POWER=100
+fi
 
 # Select correct icon based on current power and status
 if ((POWER_10 == 100)); then
@@ -25,7 +44,7 @@ ARGS=(
 )
 
 # Set text, notification level and time
-if [ "$PLUGGED" == "Discharging" ]; then
+if [[ "$PLUGGED" == "Discharging" ]]; then
   ARGS+=("Power unplugged ($POWER%)")
   if ((POWER <= 20)); then
     ARGS+=(-u critical)
@@ -39,10 +58,7 @@ else
   ARGS+=("Power plugged ($POWER%)")
 fi
 
-# My battery doesn't seem to output events when changing current capacity
-# Therefore, I just have to use a cronjob. The cronjob passes an argument
-# that avoids having a notification about battery level every 5 minutes when
-# the battery is charging
-if [ $# -eq 0 ] || ([ "$PLUGGED" == "Discharging" ] && ((POWER <= 20))); then
-  dunstify "${ARGS[@]}"
-fi
+# Debug rule to show the source of the events
+$DEBUG && [[ $# -gt 0 ]] && ARGS+=("$1")
+
+dunstify "${ARGS[@]}"
